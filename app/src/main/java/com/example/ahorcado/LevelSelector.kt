0d2863f5b24496769ce51752e7fbf9e4430520c8
CarlesPontacq <carles.pontacq@estudiant.enti.cat>
2025-11-03 10:27:59 +0100

    Splash Screen functionality made

---
+++ app/src/main/java/com/example/ahorcado/LevelSelector.kt
@@ -0,0 +1,11 @@
+package com.example.ahorcado
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LevelSelector : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level_selector)
+    }
+}
--- app/src/main/java/com/example/ahorcado/MainActivity.kt
+++ app/src/main/java/com/example/ahorcado/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.ahorcado
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var CambioEscena : LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CambioEscena = findViewById(R.id.splashScreenVerticalLayout)
+        CambioEscena.setOnClickListener{onButtonClick()}
+    }
+
+    private fun onButtonClick(){
+        val intent = Intent(this, LevelSelector::class.java);
+        startActivity(intent)
     }
 }
